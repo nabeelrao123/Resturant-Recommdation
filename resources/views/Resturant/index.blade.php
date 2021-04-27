9080c1eb2dfcfd38dc9e7ce1b3ee6c23a6f7238b
nabeelrao123 <nabeelr531@gmail.com>
2021-04-27 16:07:29 +0500

    small formatting

--- resources/views/Resturant/index.blade.php
+++ resources/views/Resturant/index.blade.php
@@ -8,8 +8,8 @@
       <div class="row mb-2">
         <div class="col-sm-6">
           <h1 class="m-0">
-        All Resturant Record
-  </h1>
+            All Resturant Record
+          </h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -25,12 +25,12 @@
   <!-- Main content -->
   <section class="content">
 
-<form method="get" action="Resturant/create">
+    <form method="get" action="Resturant/create">
 
-@method('get')
+      @method('get')
 
- <input type="submit" class="btn btn-info mb-4 " value="Create New Resturant"/>
-</form>
+      <input type="submit" class="btn btn-info mb-4 " value="Create New Resturant" />
+    </form>
     <div class="container-fluid">
       <!-- Small boxes (Stat box) -->
 
@@ -42,60 +42,60 @@
     <!--/.direct-chat -->
 
     <!-- TO DO List -->
-     
+
   </section>
   <!-- /.Left col -->
   <!-- right col (We are only adding the ID to make the widgets sortable)-->
   <section class="col-lg-12 connectedSortable">
     <div class="container">
-    <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Resturant Name</th>
-      <th scope="col">Category</th>
-      <th scope="col">Location</th>
-      <th scope="col">Action</th>
-  
-    </tr>
-  </thead>
-  <tbody>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Resturant Name</th>
+            <th scope="col">Category</th>
+            <th scope="col">Location</th>
+            <th scope="col">Action</th>
 
-@foreach($user as $data)
-    <tr>
-      <td>{{$data->id}}</td>
-      <td>{{$data->resturname}}</td>
-      <td>{{$data->category}}</td>
-      <td>{{$data->location}}</td> 
-      <td> 
-      <form  > 
-      <input type="submit" value="Show" class="btn btn-success" />
-      </form>
-      </td>
+          </tr>
+        </thead>
+        <tbody>
 
-      <td> 
-      <form action="Resturant/{{$data->id}}/edit" method="get">
+          @foreach($user as $data)
+          <tr>
+            <td>{{$data->id}}</td>
+            <td>{{$data->resturname}}</td>
+            <td>{{$data->category}}</td>
+            <td>{{$data->location}}</td>
+            <td>
+              <form>
+                <input type="submit" value="Show" class="btn btn-success" />
+              </form>
+            </td>
 
-      @method('get')
-      @csrf 
-      <input type="submit" value="Edit" class="btn btn-warning" />
-      </form>
-      </td>
-      <td> 
+            <td>
+              <form action="Resturant/{{$data->id}}/edit" method="get">
+
+                @method('get')
+                @csrf
+                <input type="submit" value="Edit" class="btn btn-warning" />
+              </form>
+            </td>
+            <td>
 
 
 
-      <form method="post" action="Resturant/{{$data->id}}">
-     @method('delete')
-     @csrf
-      <input type="submit" value="Delete" class="btn btn-danger" />
-      </form>
-      </td>
-   </tr>
- @endforeach
-   
-  </tbody>
-</table>
+              <form method="post" action="Resturant/{{$data->id}}">
+                @method('delete')
+                @csrf
+                <input type="submit" value="Delete" class="btn btn-danger" />
+              </form>
+            </td>
+          </tr>
+          @endforeach
+
+        </tbody>
+      </table>
     </div>
 
   </section>
